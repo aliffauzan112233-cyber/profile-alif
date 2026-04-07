1b8273c4862940e0db6490edc1dd8e61e80de5b5
Alif Fauzan <alif@email.com>
2026-04-07 13:52:47 +0700

    add file

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function showProjects() {
   document.getElementById("about").style.display = "none";
   document.getElementById("projects").style.display = "block";
 }
+function showProjects() {
+  const projects = document.getElementById("projects");
+  projects.classList.toggle("hidden");
+}
